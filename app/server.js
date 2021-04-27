c1352e303bc2fd6eeb9d78105e5d42e9df3e1c4e
Yusuke Kawamoto <novogrammer@gmail.com>
2021-04-27 16:52:37 +0900

    redisを追加

--- app/server.js
+++ app/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const redis = require("redis");
+const {promisify}=require("util");
+const client = redis.createClient({host:"redis"});
+const incrAsync = promisify(client.incr).bind(client);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/',async (req, res) => {
+  const count=await incrAsync("count");
+  res.send('Hello World! count:'+count);
 })
 
 app.listen(port, () => {
